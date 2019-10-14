72010db6fd9bebc950c0808dc6b9b3b6fa2f1e27
NathanYee412 <32085568+NathanYee412@users.noreply.github.com>
2019-10-14 16:36:58 -0700

    Menu
    not working

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    UnityEvent m_MyEvent;
+
+    void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+        Debug.Log("PRINT SHIT");
+        m_MyEvent.AddListener(Ping);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+}
